8979a36d675cc8c7a83bb785ce966b5c0c5795c5
liulimei <mumeiliuli@163.com>
2017-01-20 18:04:37 +0800

    autofac

--- Email.Web/Email.Web/Controllers/AccountController.cs
+++ Email.Web/Email.Web/Controllers/AccountController.cs
@@ -6,19 +6,36 @@ using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Email.Service;
 
 namespace Email.Web.Controllers
 {
     
-    public class AccountController : Controller
+    public class AccountController : BaseController
     {
+        IAdminService _service;
+        
+        public AccountController()
+        {
+
+        }
+        public AccountController(IAdminService service)
+        {
+            _service = service;
+        }
+
         // GET: Account
         public ActionResult Login()
         {
-           
-               return View();
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(AccountUser model)
+        {
+            _service.CheckUser("llm", "assd");
+            return View();
         }
 
-        
+
     }
 }
--- Email.Web/Email.Web/Scripts/main/login.js
+++ Email.Web/Email.Web/Scripts/main/login.js
@@ -1,7 +1,40 @@
 ﻿define({
-    Init: function () {
+    init: function () {
+        var self = this;
         $(".login_bg").width($(window).width());
         $(".login_bg").height($(window).height());
+        self.validate();
+    },
+    validate: function () {
+        var self = this;
+        $(".login_form").validate({
+            rules: {
+                username: {
+                    required: true,
+                },
+                password: {
+                    required: true,
+                },
+            },
+            messages: {
+                username: {
+                    required: "用户名不能为空",
+                },
+                password: {
+                    required: "密码不能为空",
+                },
+            },
+            errorPlacement: function(error, element) {  
+                $(".login_error").html(error);  
+            },
+            submitHandler: function (form) {
+                alert("提交事件!");
+                self.submit($(form));
+            }
+        });
+    },
+    submit: function ($form) {
+
     }
 
 
