54f7ee442a1113b5d4d8c0a0a77b2efa81a548fd
liulimei <mumeiliuli@163.com>
2017-01-23 13:36:10 +0800

    自定义视图和语言国际化

--- Email.Web/Email.Web/Controllers/HomeController.cs
+++ Email.Web/Email.Web/Controllers/HomeController.cs
@@ -1,17 +1,41 @@
 ﻿using Email.Util.Front;
+using Email.Web.Models;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Reflection;
+using System.Resources;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Routing;
 
 namespace Email.Web.Controllers
 {
     public class HomeController : Controller
     {
+        protected override void Initialize(RequestContext requestContext)
+        {
+            base.Initialize(requestContext);
+            if (Session["CurrentCulture"] != null)
+            {
+                Thread.CurrentThread.CurrentCulture = new CultureInfo(Session["CurrentCulture"].ToString());
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo(Session["CurrentCulture"].ToString());
+            }
+        }
+        public ActionResult ChangeCulture(string ddlCulture)
+        {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo(ddlCulture);
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo(ddlCulture);
+
+            Session["CurrentCulture"] = ddlCulture;
+            return View("Index");
+        }
         public ActionResult Index()
         {
-            return View();
+            Suggestion model = new Suggestion();
+            return View(model);
         }
 
         public ActionResult About()
