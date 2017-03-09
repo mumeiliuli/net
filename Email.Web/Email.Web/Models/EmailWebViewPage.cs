7bfc640eed0c86eb9f3c33ef1d9b0e967da8fc7e
liulimei <mumeiliuli@163.com>
2017-03-09 11:19:48 +0800

    模型

---
+++ Email.Web/Email.Web/Models/EmailWebViewPage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Email.Web.Models
+{
+    public abstract class EmailWebViewPage: EmailWebViewPage<dynamic>
+    {
+    }
+
+    public abstract class EmailWebViewPage<TModel> : WebViewPage<TModel>
+    {
+        private Type _type;
+        protected EmailWebViewPage()
+        {
+            _type = typeof(Email.International.Resource);
+        }
+        protected virtual string I(string name)
+        {
+            return _type.GetProperty(name).GetValue(null).ToString();
+        }
+        
+    }
+}
---
+++ Email.Web/Email.Web/Models/Suggestion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+
+namespace Email.Web.Models
+{
+    public class Suggestion
+    {
+        [Display(Name = "Content", ResourceType = typeof(Email.International.Resource))]
+        [Required(ErrorMessageResourceName = "ContentRequired", ErrorMessageResourceType = typeof(Email.International.Resource))]
+        public string Content { get; set; }
+    }
+}
