61e03a2de38b2ab0f59bf0bb88dbc5dd6b758849
liulimei <mumeiliuli@163.com>
2017-04-05 16:24:37 +0800

    add file

---
+++ Email.Web/Email.Web/Controllers/WxPayController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Email.Web.Controllers
+{
+    public class WxPayController : Controller
+    {
+        // GET: WxPay
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Email.Web/Email.Web/Models/TreeModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Email.Web.Models
+{
+    public class TreeModel
+    {
+        public int id { get; set; }
+        public string text { get; set; }
+        public string state { get; set; }
+
+        public string iconCls { get; set; }
+        public List<TreeModel> children { get; set; }
+    }
+}
